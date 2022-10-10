c940b57824c37b45c8cb74803b356196eac33e1e
Thaddaeus Fam <thaddaeusfamous@gmail.com>
2022-10-10 04:54:55 -0500

    task modal cleaner code

--- src/modules/taskModal.js
+++ src/modules/taskModal.js
@@ -1,49 +1,45 @@
 export const taskModal = () => {
-	const title = document.querySelector("#title");
 	const titleError = document.querySelector(".title-error");
-	const taskCancelBtn = document.querySelector(".cancel-task");
-	const taskAddBtn = document.querySelector(".add-task");
-	const showTaskModal = document.querySelector(".show-task-modal");
+	const titleInput = document.querySelector("#title");
+	const checkmark = document.querySelector(".title-checkmark-svg > svg");
+	const error = document.querySelector(".title-error-svg > svg");
 
-	const isValidTitle = () => title.checkValidity();
 	const displayError = () => {
 		titleError.textContent = "Title cannot be empty.";
 		error.style.opacity = 1;
 		titleInput.style.outline = "2px solid #ef4444";
+		checkmark.style.opacity = 0;
 	};
+
+	const displayCorrect = () => {
+		titleError.textContent = "";
+		titleInput.style.outline = "2px solid #22c55e";
+		error.style.opacity = 0;
+		checkmark.style.opacity = 1;
+	};
+
+	const title = document.querySelector("#title");
+
+	const isValidTitle = () => title.checkValidity();
+	const isValidForm = () =>
+		isValidTitle() ? toggleTaskModal() : displayError();
+
+	const taskCancelBtn = document.querySelector(".cancel-task");
+	const taskAddBtn = document.querySelector(".add-task");
+	const showTaskModal = document.querySelector(".show-task-modal");
 
 	taskCancelBtn.addEventListener("click", toggleTaskModal);
 
-	taskAddBtn.addEventListener("click", () => {
-		if (isValidTitle()) toggleTaskModal();
-		else displayError();
-	});
+	taskAddBtn.addEventListener("click", isValidForm);
 
 	showTaskModal.addEventListener("click", () => {
 		clearValues();
 		toggleTaskModal();
 	});
-	const titleInput = document.querySelector("#title");
-	const checkmark = document.querySelector(".title-checkmark-svg > svg");
-	const error = document.querySelector(".title-error-svg > svg");
 
 	title.addEventListener("keyup", () => {
-		if (isValidTitle()) {
-			titleError.textContent = "";
-			titleInput.style.outline = "2px solid #22c55e";
-			error.style.opacity = 0;
-			checkmark.style.opacity = 1;
-		} else {
-			displayError();
-			checkmark.style.opacity = 0;
-		}
+		isValidTitle() ? displayCorrect() : displayError();
 	});
-
-	const priorities = document.querySelectorAll("#priority-selected");
-
-	priorities.forEach((priority) =>
-		priority.addEventListener("click", () => console.log(priority.value))
-	);
 
 	function clearValues() {
 		const description = document.querySelector("#description");
