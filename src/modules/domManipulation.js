fccc117aefbbc35f88df9706130d1b37b4657e51
Thaddaeus Fam <thaddaeusfamous@gmail.com>
2022-10-23 07:26:24 -0500

    fix svg clicked turns text content to empty

--- src/modules/domManipulation.js
+++ src/modules/domManipulation.js
@@ -10,12 +10,7 @@ import {
 	isFuture,
 	getTaskIndex,
 } from "./task";
-import {
-	projectList,
-	filterProjectTasks,
-	getProjectId,
-	deleteProjectObject,
-} from "./project";
+import { projectList, filterProjectTasks, getProjectId } from "./project";
 
 export default function renderPage() {
 	taskModal.render();
@@ -27,7 +22,6 @@ export default function renderPage() {
 	updateProjects.render();
 	displayProjectTasks.render();
 	editProject.render();
-	deleteProject.render();
 }
 
 const taskNavigation = (function () {
@@ -39,6 +33,7 @@ const taskNavigation = (function () {
 
 		taskItemsList.addEventListener("click", (e) => {
 			if (e.target.tagName === "LI") {
+				console.log("happy bob");
 				styledTaskUpdated(e);
 				pubSub.publish("task-selected");
 			}
@@ -432,7 +427,6 @@ export const taskCard = (function () {
 
 	const getTaskArray = () => {
 		const taskIndex = getTaskNameIndex();
-
 		const selectedTask = document.querySelector(".task-selected");
 		const projectSelected = selectedTask.classList.contains("project-item");
 
@@ -442,7 +436,7 @@ export const taskCard = (function () {
 
 		const projectDataSetId = selectedTask.dataset.projectId;
 		const projectTask =
-			selectedTask.children[1].textContent.trim() + projectDataSetId;
+			selectedTask.children[1].textContent + projectDataSetId;
 
 		return filterProjectTasks(projectTask);
 	};
@@ -640,12 +634,11 @@ const projectNavigation = (function () {
 	const selectedProject = (e) => {
 		taskNavigation.selectedItem().classList.remove("task-selected");
 		e.target.closest("li").classList.add("task-selected");
+		const projectName = document.querySelector(
+			".task-selected .project-name"
+		);
 
-		if (!e.target.classList.contains("project-setting"))
-			taskNavigation.taskHeader().textContent = e.target.textContent;
-		else
-			taskNavigation.taskHeader().textContent =
-				e.target.previousElementSibling.textContent;
+		taskNavigation.taskHeader().textContent = projectName.textContent;
 	};
 
 	return { render };
@@ -720,7 +713,7 @@ const createDomProject = (function () {
 		);
 	};
 
-	const projectSettingSvg = (projectUtilitiesRight) => {
+	const projectSettingSvg = (projectItem) => {
 		const projectSettingEl = document.createElement("span");
 		projectSettingEl.classList.add("project-setting");
 		projectSettingEl.innerHTML = `<svg id="task-edit" style="width:20px;height:20px" viewBox="0 0 24 24">
@@ -728,8 +721,9 @@ const createDomProject = (function () {
 			d="M12,8A4,4 0 0,1 16,12A4,4 0 0,1 12,16A4,4 0 0,1 8,12A4,4 0 0,1 12,8M12,10A2,2 0 0,0 10,12A2,2 0 0,0 12,14A2,2 0 0,0 14,12A2,2 0 0,0 12,10M10,22C9.75,22 9.54,21.82 9.5,21.58L9.13,18.93C8.5,18.68 7.96,18.34 7.44,17.94L4.95,18.95C4.73,19.03 4.46,18.95 4.34,18.73L2.34,15.27C2.21,15.05 2.27,14.78 2.46,14.63L4.57,12.97L4.5,12L4.57,11L2.46,9.37C2.27,9.22 2.21,8.95 2.34,8.73L4.34,5.27C4.46,5.05 4.73,4.96 4.95,5.05L7.44,6.05C7.96,5.66 8.5,5.32 9.13,5.07L9.5,2.42C9.54,2.18 9.75,2 10,2H14C14.25,2 14.46,2.18 14.5,2.42L14.87,5.07C15.5,5.32 16.04,5.66 16.56,6.05L19.05,5.05C19.27,4.96 19.54,5.05 19.66,5.27L21.66,8.73C21.79,8.95 21.73,9.22 21.54,9.37L19.43,11L19.5,12L19.43,13L21.54,14.63C21.73,14.78 21.79,15.05 21.66,15.27L19.66,18.73C19.54,18.95 19.27,19.04 19.05,18.95L16.56,17.95C16.04,18.34 15.5,18.68 14.87,18.93L14.5,21.58C14.46,21.82 14.25,22 14,22H10M11.25,4L10.88,6.61C9.68,6.86 8.62,7.5 7.85,8.39L5.44,7.35L4.69,8.65L6.8,10.2C6.4,11.37 6.4,12.64 6.8,13.8L4.68,15.36L5.43,16.66L7.86,15.62C8.63,16.5 9.68,17.14 10.87,17.38L11.24,20H12.76L13.13,17.39C14.32,17.14 15.37,16.5 16.14,15.62L18.57,16.66L19.32,15.36L17.2,13.81C17.6,12.64 17.6,11.37 17.2,10.2L19.31,8.65L18.56,7.35L16.15,8.39C15.38,7.5 14.32,6.86 13.12,6.62L12.75,4H11.25Z" />
 	</svg>`;
 
-		projectUtilitiesRight.appendChild(projectSettingEl);
+		projectItem.appendChild(projectSettingEl);
 		projectSettingEl.addEventListener("click", (e) => {
+			pubSub.publish("project-item-clicked", e);
 			pubSub.publish("project-setting-clicked", e);
 		});
 	};
@@ -744,39 +738,10 @@ const createDomProject = (function () {
 	return { render };
 })();
 
-const deleteProject = (function () {
-	const render = () => {
-		pubSub.subscribe("project-delete-clicked", deleteDisplayProject);
-	};
-
-	const deleteDisplayProject = (e) => {
-		e.stopPropagation();
-		const projectToDelete = e.target.closest(".project-item");
-		projectToDelete.remove();
-		deleteProjectObject(e);
-		pubSub.publish("project-deleted");
-	};
-
-	return { render };
-})();
-
-const updateProjects = (function () {
-	const render = () => {
-		pubSub.subscribe("project-created", updateProjectCounter);
-		pubSub.subscribe("project-deleted", updateProjectCounter);
-	};
-
-	const updateProjectCounter = () => {
-		const projectCounter = document.querySelector(".projects-counter");
-		projectCounter.textContent = projectList.length;
-	};
-
-	return { render };
-})();
-
 const editProject = (function () {
 	const render = () => {
 		pubSub.subscribe("project-setting-clicked", (e) => {
+			e.stopPropagation();
 			toggleEditProjectModal();
 			editProjectResetStyling();
 			editProjectCurrentInputTitle(e);
@@ -854,8 +819,10 @@ const editProject = (function () {
 	};
 
 	const currentProjectTitle = (e) => {
-		const projectName = e.target.previousElementSibling;
-		return projectName.textContent;
+		const projectSelectedName = document.querySelector(
+			".task-selected  .project-name"
+		);
+		return projectSelectedName.textContent;
 	};
 
 	const editProjectCurrentInputTitle = (e) => {
@@ -869,6 +836,19 @@ const editProject = (function () {
 		);
 
 		projectName.textContent = projectList[id].name;
+	};
+
+	return { render };
+})();
+
+const updateProjects = (function () {
+	const render = () => {
+		pubSub.subscribe("project-created", updateProjectCounter);
+	};
+
+	const updateProjectCounter = () => {
+		const projectCounter = document.querySelector(".projects-counter");
+		projectCounter.textContent = projectList.length;
 	};
 
 	return { render };
--- src/modules/project.js
+++ src/modules/project.js
@@ -10,8 +10,8 @@ const projectList = [];
 pubSub.subscribe("project-name-submitted", createProject);
 
 function createProject() {
-	const projectName = document.querySelector("#project-input").value.trim();
-	console.log(projectName);
+	const projectName = document.querySelector("#project-input").value;
+
 	projectList.push(Project(projectName, projectList.length));
 	pubSub.publish("project-created", Project(projectName, projectList.length));
 	console.log(projectList);
@@ -22,64 +22,30 @@ pubSub.subscribe("task-created", createProjectTask);
 function getProjectId() {
 	const projectSelected = document.querySelector(".task-selected");
 	const projectDataSetId = projectSelected.dataset.projectId;
-
+	console.log(projectSelected, projectDataSetId);
 	return projectDataSetId;
 }
 
 function createProjectTask(task) {
-	// if (!getProjectId().classList.contains("project-item")) return;
-	// if (!getProjectId()) return;
-	const projectSelected = document.querySelector(".task-selected");
-	const projectDataSetId = projectSelected.dataset.projectId;
-
-	if (!projectSelected.classList.contains("project-item")) return;
+	console.log(getProjectId());
+	const isProjectSelected = document.querySelector(".task-selected");
+	if (!isProjectSelected.classList.contains("project-item")) return;
 
 	const projectSelectedName = document.querySelector(".task-selected div");
 	const projectTask = tasks[task.id];
 
-	projectTask.projectName =
-		projectSelectedName.textContent.trim() + projectDataSetId;
-	console.log(projectList, tasks);
+	projectTask.projectName = projectSelectedName.textContent + getProjectId();
 }
 
 pubSub.subscribe("edited-project", updateProject);
 
 function updateProject(newName) {
-	projectList[getProjectId()].name = newName.trim();
+	projectList[getProjectId()].name = newName;
 	console.log(projectList);
 	pubSub.publish("updated-project-name", getProjectId());
 }
 
-function deleteProjectObject(e) {
-	const projectDeleted = e.target.closest(".project-item");
-	const objectProjectId = projectDeleted.dataset.projectId;
-
-	projectList[objectProjectId] = "";
-	projectList.splice(objectProjectId, 1);
-
-	recorrectObjectId();
-	recorrectProjectDataSetId();
-}
-
-function recorrectObjectId() {
-	let i = -1;
-	Object.keys(projectList).forEach((key) => (projectList[key].id = i += 1));
-}
-
-function recorrectProjectDataSetId() {
-	const projectItem = document.querySelectorAll(".project-item");
-	let i = -1;
-	projectItem.forEach((project) => {
-		project.dataset.projectId = i += 1;
-	});
-}
 const filterProjectTasks = (projectSelectedName) =>
 	tasks.filter((item) => item.projectName === projectSelectedName);
 
-export {
-	projectList,
-	createProjectTask,
-	filterProjectTasks,
-	getProjectId,
-	deleteProjectObject,
-};
+export { projectList, createProjectTask, filterProjectTasks, getProjectId };
