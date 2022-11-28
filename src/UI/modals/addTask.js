5f0b1f77b63e327207c942fa6b66fe94814a38e2
Thaddaeus Fam <103373668+cColds@users.noreply.github.com>
2022-11-28 15:00:33 -0600

    Add title verification for add task modal

--- src/UI/modals/addTask.js
+++ src/UI/modals/addTask.js
@@ -1,12 +1,41 @@
 const modal = document.querySelector(".modal");
-const modalOverlay = document.querySelector(".modal-overlay");
+const overlayModal = document.querySelector(".modal-overlay");
 const openModal = document.querySelector("#add-task");
-const cancelModal = document.querySelector("#add-task-cancel");
-const addModal = document.querySelector("#add-task-add");
+const cancelBtn = document.querySelector("#add-task-cancel");
+const closeBtn = document.querySelector(".modal-header-cancel");
+const add = document.querySelector("#add-task-add");
 
-openModal.addEventListener("click", () => {
+const title = document.querySelector("#add-task-title");
+const titleError = document.querySelector(".title-error");
+
+const toggleError = () => {
+	if (title.value) {
+		title.classList.remove("active");
+		titleError.classList.remove("active");
+	} else {
+		title.classList.add("active");
+		titleError.classList.add("active");
+	}
+};
+
+title.addEventListener("keyup", () => {
+	toggleError();
+});
+
+const toggleModal = () => {
 	modal.classList.toggle("active");
-	modalOverlay.classList.toggle("active");
+	overlayModal.classList.toggle("active");
+};
+
+openModal.addEventListener("click", toggleModal);
+cancelBtn.addEventListener("click", toggleModal);
+closeBtn.addEventListener("click", toggleModal);
+add.addEventListener("click", () => {
+	if (title.value) {
+		toggleModal();
+	} else {
+		toggleError();
+	}
 });
 
 let egg = 1;
