1b6afe3d7bcaaa64fec0868d93d2c63b72747f83
Thaddaeus Fam <103373668+cColds@users.noreply.github.com>
2022-12-06 11:15:32 -0600

    Add due date to dom and change edit icon

--- src/UI/task/populateTasks.js
+++ src/UI/task/populateTasks.js
@@ -11,36 +11,71 @@ const populateTask = (task) => {
 	const allTasks = document.querySelector(".all-tasks");
 
 	const taskContainer = document.createElement("div");
-	const completeTask = document.createElement("div");
+	const taskContentLeft = document.createElement("div");
+	const taskContentRight = document.createElement("div");
 	const titleDescriptionContainer = document.createElement("div");
-	const titleTask = document.createElement("div");
-	const descriptionTask = document.createElement("div");
 	const editTaskContainer = document.createElement("div");
 
-	helperFunction(taskContainer, ["task", task.priority]);
+	const completeTask = document.createElement("div");
+	const titleTask = document.createElement("div");
+	const descriptionTask = document.createElement("div");
+	const dueDateTask = document.createElement("div");
+
+	taskContentLeft.classList.add("task-content-left");
+	taskContentRight.classList.add("task-content-right");
 	completeTask.classList.add("complete-task");
 	editTaskContainer.classList.add("edit-task-container");
 	titleDescriptionContainer.classList.add("text-task");
+	dueDateTask.classList.add("due-date-task");
+
+	helperFunction(taskContainer, ["task", task.priority]);
 	helperFunction(titleTask, ["title-task"], task.title);
 	helperFunction(descriptionTask, ["description-task"], task.description);
+
+	dueDateTask.textContent =
+		task.dueDate.toString() !== "Invalid Date"
+			? task.dueDate
+			: "No Due Date";
+
 	editTaskContainer.innerHTML += editTaskIcon();
 
 	titleDescriptionContainer.append(titleTask, descriptionTask);
-	taskContainer.append(
-		completeTask,
-		titleDescriptionContainer,
-		editTaskContainer
-	);
+	taskContentLeft.append(completeTask, titleDescriptionContainer);
+	taskContentRight.append(dueDateTask, editTaskContainer);
+	taskContainer.append(taskContentLeft, taskContentRight);
 
 	allTasks.appendChild(taskContainer);
 };
 
 const editTaskIcon = () => {
 	return `<svg class="edit edit-task" style="width:24px;height:24px" viewBox="0 0 24 24">
-    <path fill="currentColor"
-        d="M11 20.1L19.2 11.9C19.7 11.4 20.3 11.1 21 11.1C21.7 11.1 22.3 11.4 22.8 11.9L23 12.1V4C23 2.9 22.1 2 21 2H3C1.9 2 1 2.9 1 4V20C1 21.1 1.9 22 3 22H11V20.1M3 4H21V7H3V4M21 13.1C20.9 13.1 20.7 13.2 20.6 13.3L19.6 14.3L21.7 16.4L22.7 15.4C22.9 15.2 22.9 14.8 22.7 14.6L21.4 13.3C21.3 13.2 21.2 13.1 21 13.1M19.1 14.9L13 20.9V23H15.1L21.2 16.9L19.1 14.9Z" />
+    <path fill="currentColor" d="M21.7 13.35L20.7 14.35L18.65 12.35L19.65 11.35C19.85 11.14 20.19 11.13 20.42 11.35L21.7 12.63C21.89 12.83 21.89 13.15 21.7 13.35M12 18.94V21H14.06L20.12 14.88L18.07 12.88L12 18.94M5 19H10V21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H6V1H8V3H16V1H18V3H19C20.11 3 21 3.9 21 5V9H5V19M5 5V7H19V5H5Z" />
 </svg>`;
 };
+
+{
+	/* <div class="task">
+
+<!-- <div class="complete-task"></div> -->
+
+<div class="task-content-left">
+    <div class="complete-task"></div>
+    <div class="text-task">
+        <div class="title-task">Title test</div>
+        <div class="description-task">Apples are red</div>
+    </div>
+</div>
+<div class="task-content-right">
+    <div class="due-date-task">December 1st, 2022</div>
+    <div class="edit-task-container">
+        <svg class="edit-task" style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path fill="currentColor"
+                d="M21.7 13.35L20.7 14.35L18.65 12.35L19.65 11.35C19.85 11.14 20.19 11.13 20.42 11.35L21.7 12.63C21.89 12.83 21.89 13.15 21.7 13.35M12 18.94V21H14.06L20.12 14.88L18.07 12.88L12 18.94M5 19H10V21H5C3.9 21 3 20.11 3 19V5C3 3.9 3.9 3 5 3H6V1H8V3H16V1H18V3H19C20.11 3 21 3.9 21 5V9H5V19M5 5V7H19V5H5Z" />
+        </svg>
+    </div>
+</div>
+</div> */
+}
 
 const helperFunction = (element, classAttribute, text) => {
 	classAttribute.forEach((attr) => element.classList.add(attr));
