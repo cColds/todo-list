39707a0e613699135a6e8b8dc36cbf45a0bcbc9b
Thaddaeus Fam <103373668+cColds@users.noreply.github.com>
2022-12-07 06:49:09 -0600

    Refactor week filter to prevent due date before today

--- src/AppLogic/task.js
+++ src/AppLogic/task.js
@@ -1,4 +1,4 @@
-import { format, isToday, differenceInDays } from "date-fns";
+import { format, isToday, differenceInDays, isYesterday } from "date-fns";
 import { pubSub } from "../pubsub";
 import { getProjectName } from "../UI/navigation/switchProject.js";
 
@@ -65,29 +65,11 @@ const filterWeek = () => {
 	const weekTaskList = [];
 	taskList.forEach((task) => {
 		const currentTime = new Date();
+		const week = differenceInDays(task.dueDate, currentTime);
 
-		if (differenceInDays(task.dueDate, currentTime) <= 7)
-			weekTaskList.push(task);
+		if (week <= 7 && week >= 0) weekTaskList.push(task);
 	});
 	pubSub.publish("filter-task", weekTaskList);
 };
-
-//Wed Dec 07 2022 00:22:58 GMT-0600 (Central Standard Time)
-// let defaultDate = new Date();
-
-// console.log(format(defaultDate, "EEE, LLLL do, y"));
-
-//2022-12-07T03:28
-
-// let defaultDate = new Date("2022-12-06T03:28");
-// console.log(isToday(defaultDate));
-// console.log(defaultDate);
-// console.log(addDays(defaultDate, 1));
-// console.log(differenceInDays(addDays(defaultDate, 7), defaultDate));
-
-// console.log(differenceInDays(defaultDate));
-// console.log(isToday(defaultDate));
-// console.log(isToday(format(defaultDate, "EEE, LLLL do, y p")));
-// console.log(isToday(format(defaultDate, "MM/dd/yyyy")));
 
 export { addTask, completeTask, editTask, taskList, updateId };
