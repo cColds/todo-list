470f32a4887a07f081d11e77732396f56d93382c
Thaddaeus Fam <103373668+cColds@users.noreply.github.com>
2022-12-08 17:41:18 -0600

    Split code functionality to their own function for readability in switchProject

--- src/UI/navigation/switchProject.js
+++ src/UI/navigation/switchProject.js
@@ -9,17 +9,28 @@ const getMainProjectId = () => {
 const mainProjects = document.querySelector("#main-projects-list");
 
 mainProjects.addEventListener("click", (e) => {
-	const previousSelected = document.querySelector(".selected");
-	previousSelected.classList.remove("selected");
+	unselectPreviousProject();
+	selectCurrentProject(e);
+	updateMainTitle();
 
+	pubSub.publish("main-project-switched");
+});
+
+const unselectPreviousProject = () => {
+	const previousProject = document.querySelector(".selected");
+	previousProject.classList.remove("selected");
+};
+
+const selectCurrentProject = (e) => {
 	e.target.closest(".projects-item").classList.add("selected");
+};
 
+const updateMainTitle = () => {
 	const currentSelectedTitle = document.querySelector(
 		".selected .projects-item-name"
 	);
 	const mainTitle = document.querySelector("#main-title");
 	mainTitle.textContent = currentSelectedTitle.textContent;
-	pubSub.publish("main-project-switched");
-});
+};
 
 export { mainProjects, getProjectId, getMainProjectId };
