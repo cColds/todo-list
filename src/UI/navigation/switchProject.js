e12167b741a99a80b3e4520bd72bc1f493e20c8d
Thaddaeus Fam <103373668+cColds@users.noreply.github.com>
2022-12-08 21:06:57 -0600

    Added functionality to check main or regular projects for navigation

--- src/UI/navigation/switchProject.js
+++ src/UI/navigation/switchProject.js
@@ -3,18 +3,31 @@ import { pubSub } from "../../pubsub";
 const getProjectId = () => {
 	return +document.querySelector(".selected").dataset.projectId;
 };
+
 const getMainProjectId = () => {
 	return +document.querySelector(".selected").dataset.mainProjectId;
 };
+
 const mainProjects = document.querySelector("#main-projects-list");
 
-mainProjects.addEventListener("click", (e) => {
+mainProjects.addEventListener("click", (e) => switchProject(e));
+
+const switchProject = (e) => {
+	console.log(e.target);
 	unselectPreviousProject();
 	selectCurrentProject(e);
-	updateMainTitle();
 
-	pubSub.publish("main-project-switched");
-});
+	updateMainTitle();
+	pubSub.publish(getProjectType());
+};
+
+const getProjectType = () => {
+	const selectedProject = document.querySelector(".selected");
+	const isMain = selectedProject.classList
+		.toString()
+		.includes("main-project");
+	return isMain ? "main-project-switched" : "project-switched";
+};
 
 const unselectPreviousProject = () => {
 	const previousProject = document.querySelector(".selected");
@@ -23,6 +36,7 @@ const unselectPreviousProject = () => {
 
 const selectCurrentProject = (e) => {
 	e.target.closest(".projects-item").classList.add("selected");
+	console.log(e.target.closest(".projects-item"));
 };
 
 const updateMainTitle = () => {
@@ -34,3 +48,5 @@ const updateMainTitle = () => {
 };
 
 export { mainProjects, getProjectId, getMainProjectId };
+
+// make project nav work
